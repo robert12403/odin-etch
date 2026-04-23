51f46237a4d0a7ab86cf0e984dcc7ebf794d48e8
robert12403 <robert.chiriac1998@icloud.com>
2026-04-23 09:39:28 +0300

    New grid now allows maximum 100
    The button won't allow generating grinds over 100x100, preventing crashes.

--- javascript.js
+++ javascript.js
@@ -1,19 +1,22 @@
 
 function makeGrid(nr) {
-    const container = document.querySelector("#container")
-    const square = document.querySelectorAll(".square-style")
-    square.forEach(square => square.remove())
-    for (let i = 0; i < nr; i++) {
-        const squareLine = document.createElement("div")
-        squareLine.classList.add("square-line")
-        container.appendChild(squareLine)
-        for (let j = 0; j < nr; j++) {
-            const square = document.createElement("div")
-            square.addEventListener("mouseenter", () => square.style.backgroundColor = "blue")
-            square.classList.add("square-style")
-            squareLine.appendChild(square)
+    if (nr <= 100) {
+        const container = document.querySelector("#container")
+        const square = document.querySelectorAll(".square-style")
+        square.forEach(square => square.remove())
+        for (let i = 0; i < nr; i++) {
+            const squareLine = document.createElement("div")
+            squareLine.classList.add("square-line")
+            container.appendChild(squareLine)
+            for (let j = 0; j < nr; j++) {
+                const square = document.createElement("div")
+                square.addEventListener("mouseenter", () => square.style.backgroundColor = "blue")
+                square.classList.add("square-style")
+                squareLine.appendChild(square)
+            }
         }
     }
+    else alert("Maximum 100x100 allowed.")
 }
 
 makeGrid(16)
